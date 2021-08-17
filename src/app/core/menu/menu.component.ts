659ca5e2fce9805caf8d500e955ef918a4bdde7c
Michelle Jowers <michelle.jowers@icloud.com>
2021-08-17 12:27:22 -0400

    update review button visibility

--- src/app/core/menu/menu.component.ts
+++ src/app/core/menu/menu.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { RequestReviewComponent } from 'src/app/feature/request/request-review/request-review.component';
 import { MenuItem } from 'src/app/model/menu-item.class';
+import { User } from 'src/app/model/user.class';
+import { SystemService } from 'src/app/service/system.service';
 
 @Component({
   selector: 'app-menu',
@@ -9,20 +12,31 @@ import { MenuItem } from 'src/app/model/menu-item.class';
 export class MenuComponent implements OnInit {
 
   menuItems: MenuItem[] = [];
+  
+  loggedInUser: User = new User();
+  
 
-  constructor() { }
+  constructor(
+    private systemSvc: SystemService
+  ) { }
 
   ngOnInit(): void {
+
+    this.loggedInUser = this.systemSvc.loggedInUser;
+
     this.menuItems = [
       new MenuItem("User", "/user-list", "User List"),
       new MenuItem("Vendor", "/vendor-list", "Vendor List"),
-      new MenuItem("Product", "/product-list", "Prodcut List"),
+      new MenuItem("Product", "/product-list", "Product List"),
       new MenuItem("Request", "/request-list", "Request List"),
-      new MenuItem("Review", "/request-review", "Review Items" ),
       new MenuItem("Logout", "/user-login", "User Login")
-   
     ]
-  }
-  
+    //is user reviewer?  if so, add Review menu component
 
+    let reviewer = this.systemSvc.loggedInUser.reviewer;
+    if (reviewer == true)
+    this.menuItems.push(new MenuItem("Review", "/request-review", "Review"));
+  }
 }
+
+  
